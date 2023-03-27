44845f1fdfb133ba93a236a2ea9efe3b07151350
kimchankyo <chris.chankyo.kim@gmail.com>
2023-03-26 22:07:12 -0700

    Added deck class

--- src/components.py
+++ src/components.py
@@ -1,7 +1,6 @@
 from typing import Tuple, List, Dict
 import numpy as np
-import time
-import sys
+import random
 
 
 """Classes"""
@@ -12,6 +11,8 @@ class Suit:
   HEARTS = (1, '\u2665')
   DIAMONDS = (2, '\u2666')
   CLUBS = (3, '\u2667')
+
+  asList = [SPADES, HEARTS, DIAMONDS, CLUBS]
 
 
 class Rank:
@@ -38,8 +39,8 @@ class Rank:
 class Card:
   def __init__(self, rank: Tuple[int, str], 
                suit: Tuple[int, str]) -> None:
-    suitBit, self._suit = suit
-    grade, self._value = rank
+    suitBit, self.suit = suit
+    grade, self.value = rank
     self._sig = self._constructSignature(suitBit, grade)
 
   def _constructSignature(self, suitBit: int, grade: int) -> int:
@@ -57,3 +58,31 @@ class Card:
   
   def getSignature(self) -> int:
     return self._sig
+
+
+# Deck
+class Deck:
+  def __init__(self) -> None:
+    self._stack = [Card(r, s) for s in Suit.asList for r in Rank.asList]
+    self._loc = 0
+    random.shuffle(self._stack)
+
+  def __len__(self) -> int:
+    return len(self._stack) - self._loc
+
+  def canDraw(self) -> bool:
+    return self._loc < len(self._stack)
+
+  def countRemaining(self) -> int:
+    return self.__len__()
+
+  def draw(self) -> Card:
+    if self.canDraw():
+      card = self._stack[self._loc]
+      self._loc += 1
+      return card
+    return None      # TODO: Proper error handling
+
+  def reset(self) -> None:
+    random.shuffle(self._stack)
+    self._loc = 0
